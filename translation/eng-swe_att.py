8660508d879a476864be305945eeb89e80aee905
prashu316 <prashanth.panther@gmail.com>
2022-03-18 12:07:45 +0530

    encoder and attention layer

--- translation/eng-swe_att.py
+++ translation/eng-swe_att.py
@@ -1,17 +1,58 @@
 import numpy as np
-
 import typing
 from typing import Any, Tuple
-
 import tensorflow as tf
-
 import tensorflow_text as tf_text
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
-lines= pd.read_table('dataset.txt',  names =['inp', 'targ', 'comments'])
 
+#Utility class for checking shape
+class ShapeChecker():
+  def __init__(self):
+    # Keep a cache of every axis-name seen
+    self.shapes = {}
+
+  def __call__(self, tensor, names, broadcast=False):
+    if not tf.executing_eagerly():
+      return
+
+    if isinstance(names, str):
+      names = (names,)
+
+    shape = tf.shape(tensor)
+    rank = tf.rank(tensor)
+
+    if rank != len(names):
+      raise ValueError(f'Rank mismatch:\n'
+                       f'    found {rank}: {shape.numpy()}\n'
+                       f'    expected {len(names)}: {names}\n')
+
+    for i, name in enumerate(names):
+      if isinstance(name, int):
+        old_dim = name
+      else:
+        old_dim = self.shapes.get(name, None)
+      new_dim = shape[i]
+
+      if (broadcast and new_dim == 1):
+        continue
+
+      if old_dim is None:
+        # If the axis name is new, add its length to the cache.
+        self.shapes[name] = new_dim
+        continue
+
+      if new_dim != old_dim:
+        raise ValueError(f"Shape mismatch for dimension: '{name}'\n"
+                         f"    found: {new_dim}\n"
+                         f"    expected: {old_dim}\n")
+
+
+lines= pd.read_table('swe.txt',  names =['inp', 'targ', 'comments'])
+inp=lines.inp
+targ=lines.targ
 BUFFER_SIZE = len(lines.inp)
 BATCH_SIZE = 64
 
@@ -19,7 +60,182 @@ dataset = tf.data.Dataset.from_tensor_slices((lines.inp, lines.targ)).shuffle(BU
 dataset = dataset.batch(BATCH_SIZE)
 
 for example_input_batch, example_target_batch in dataset.take(1):
-  print(example_input_batch[:10])
+  print(example_input_batch[:5])
   print()
   print(example_target_batch[:5])
   break
+
+#text preprocessing
+example_text = tf.constant('Jag börjar aldrig slåss')
+
+print(example_text.numpy())
+print(tf_text.normalize_utf8(example_text, 'NFKD').numpy())
+
+def tf_lower_and_split_punct(text):
+  # Split accecented characters.
+  text = tf_text.normalize_utf8(text, 'NFKD')
+  text = tf.strings.lower(text)
+  # Keep space, a to z, and select punctuation.
+  text = tf.strings.regex_replace(text, '[^ a-z.?!,¿]', '')
+  # Add spaces around punctuation.
+  text = tf.strings.regex_replace(text, '[.?!,¿]', r' \0 ')
+  # Strip whitespace.
+  text = tf.strings.strip(text)
+
+  text = tf.strings.join(['[START]', text, '[END]'], separator=' ')
+  return text
+
+print(example_text.numpy().decode())
+print(tf_lower_and_split_punct(example_text).numpy().decode())
+
+#text vectorization
+
+max_vocab_size = 5000
+
+#english
+input_text_processor = tf.keras.layers.TextVectorization(
+    standardize=tf_lower_and_split_punct,
+    max_tokens=max_vocab_size)
+
+#adapt english vocab
+input_text_processor.adapt(inp)
+print(input_text_processor.get_vocabulary()[:10])
+
+#swedish
+output_text_processor = tf.keras.layers.TextVectorization(
+    standardize=tf_lower_and_split_punct,
+    max_tokens=max_vocab_size)
+
+output_text_processor.adapt(targ)
+print(output_text_processor.get_vocabulary()[:10])
+
+#print token converted strings
+example_tokens = input_text_processor(example_input_batch)
+#print(example_tokens[:3, :10])
+
+
+#get vocab for conerting back to string
+input_vocab = np.array(input_text_processor.get_vocabulary())
+tokens = input_vocab[example_tokens[0].numpy()]
+print(' '.join(tokens))
+
+#the returned token ids are 0 padded
+plt.subplot(1, 2, 1)
+plt.pcolormesh(example_tokens)
+plt.title('Token IDs')
+
+plt.subplot(1, 2, 2)
+plt.pcolormesh(example_tokens != 0)
+plt.title('Mask')
+#plt.show()
+
+embedding_dim = 256
+units = 1024
+
+#defining the encoder using subclassing
+#ENCODER
+class Encoder(tf.keras.layers.Layer):
+  def __init__(self, input_vocab_size, embedding_dim, enc_units):
+    super(Encoder, self).__init__()
+    self.enc_units = enc_units
+    self.input_vocab_size = input_vocab_size
+
+    # The embedding layer converts tokens to vectors
+    self.embedding = tf.keras.layers.Embedding(self.input_vocab_size,
+                                               embedding_dim)
+
+    # The GRU RNN layer processes those vectors sequentially.
+    self.gru = tf.keras.layers.GRU(self.enc_units,
+                                   # Return the sequence and state
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_initializer='glorot_uniform')
+
+  def call(self, tokens, state=None):
+    shape_checker = ShapeChecker()
+    shape_checker(tokens, ('batch', 's'))
+
+    # 2. The embedding layer looks up the embedding for each token.
+    vectors = self.embedding(tokens)
+    shape_checker(vectors, ('batch', 's', 'embed_dim'))
+
+    # 3. The GRU processes the embedding sequence.
+    #    output shape: (batch, s, enc_units)
+    #    state shape: (batch, enc_units)
+    output, state = self.gru(vectors, initial_state=state)
+    shape_checker(output, ('batch', 's', 'enc_units'))
+    shape_checker(state, ('batch', 'enc_units'))
+
+    # 4. Returns the new sequence and its state.
+    return output, state
+
+#using the encoder on our example batch
+# Convert the input text to tokens.
+example_tokens = input_text_processor(example_input_batch)
+
+# Encode the input sequence.
+encoder = Encoder(input_text_processor.vocabulary_size(),
+                  embedding_dim, units)
+example_enc_output, example_enc_state = encoder(example_tokens)
+
+#checking the shape at every layer
+'''
+print(f'Input batch, shape (batch): {example_input_batch.shape}')
+print(f'Input batch tokens, shape (batch, s): {example_tokens.shape}')
+print(f'Encoder output, shape (batch, s, units): {example_enc_output.shape}')
+print(f'Encoder state, shape (batch, units): {example_enc_state.shape}')
+'''
+
+#The ATTENTION LAYER
+
+class BahdanauAttention(tf.keras.layers.Layer):
+  def __init__(self, units):
+    super().__init__()
+    # For Eqn. (4), the  Bahdanau attention
+    self.W1 = tf.keras.layers.Dense(units, use_bias=False)
+    self.W2 = tf.keras.layers.Dense(units, use_bias=False)
+
+    self.attention = tf.keras.layers.AdditiveAttention()
+
+  def call(self, query, value, mask):
+    shape_checker = ShapeChecker()
+    shape_checker(query, ('batch', 't', 'query_units'))
+    shape_checker(value, ('batch', 's', 'value_units'))
+    shape_checker(mask, ('batch', 's'))
+
+    # From Eqn. (4), `W1@ht`.
+    w1_query = self.W1(query)
+    shape_checker(w1_query, ('batch', 't', 'attn_units'))
+
+    # From Eqn. (4), `W2@hs`.
+    w2_key = self.W2(value)
+    shape_checker(w2_key, ('batch', 's', 'attn_units'))
+
+    query_mask = tf.ones(tf.shape(query)[:-1], dtype=bool)
+    value_mask = mask
+
+    context_vector, attention_weights = self.attention(
+        inputs = [w1_query, value, w2_key],
+        mask=[query_mask, value_mask],
+        return_attention_scores = True,
+    )
+    shape_checker(context_vector, ('batch', 't', 'value_units'))
+    shape_checker(attention_weights, ('batch', 't', 's'))
+
+    return context_vector, attention_weights
+
+#testing it on example input
+attention_layer = BahdanauAttention(units)
+# Later, the decoder will generate this attention query
+example_attention_query = tf.random.normal(shape=[len(example_tokens), 2, 10])
+
+# Attend to the encoded tokens
+
+context_vector, attention_weights = attention_layer(
+    query=example_attention_query,
+    value=example_enc_output,
+    mask=(example_tokens != 0))
+
+print(f'Attention result shape: (batch_size, query_seq_length, units):           {context_vector.shape}')
+print(f'Attention weights shape: (batch_size, query_seq_length, value_seq_length): {attention_weights.shape}')
+
