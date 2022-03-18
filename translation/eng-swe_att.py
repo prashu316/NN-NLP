6ee6da993a8262667bc64000daebf2249fb6a950
prashu316 <prashanth.panther@gmail.com>
2022-03-18 20:11:53 +0530

    final model

--- translation/eng-swe_att.py
+++ translation/eng-swe_att.py
@@ -582,9 +582,160 @@ class BatchLogs(tf.keras.callbacks.Callback):
 batch_loss = BatchLogs('batch_loss')
 
 
-train_translator.fit(dataset, epochs=10, callbacks=[batch_loss])
-train_translator.save_weights('attention_model_2')
-#train_translator.load_weights('attention_model_1')
+#train_translator.fit(dataset, epochs=10, callbacks=[batch_loss])
+#train_translator.save_weights('attention_model_2')
+train_translator.load_weights('attention_model_2')
 '''
 model.load_weights('./checkpoints/my_checkpoint')
 '''
+
+#INFERENCE
+class Translator(tf.Module):
+
+  def __init__(self, encoder, decoder, input_text_processor,
+               output_text_processor):
+    self.encoder = encoder
+    self.decoder = decoder
+    self.input_text_processor = input_text_processor
+    self.output_text_processor = output_text_processor
+
+    self.output_token_string_from_index = (
+        tf.keras.layers.StringLookup(
+            vocabulary=output_text_processor.get_vocabulary(),
+            mask_token='',
+            invert=True))
+
+    # The output should never generate padding, unknown, or start.
+    index_from_string = tf.keras.layers.StringLookup(
+        vocabulary=output_text_processor.get_vocabulary(), mask_token='')
+    token_mask_ids = index_from_string(['', '[UNK]', '[START]']).numpy()
+
+    token_mask = np.zeros([index_from_string.vocabulary_size()], dtype=np.bool)
+    token_mask[np.array(token_mask_ids)] = True
+    self.token_mask = token_mask
+
+    self.start_token = index_from_string(tf.constant('[START]'))
+    self.end_token = index_from_string(tf.constant('[END]'))
+
+translator = Translator(
+    encoder=train_translator.encoder,
+    decoder=train_translator.decoder,
+    input_text_processor=input_text_processor,
+    output_text_processor=output_text_processor,
+)
+
+#converting IDS to text
+def tokens_to_text(self, result_tokens):
+  shape_checker = ShapeChecker()
+  shape_checker(result_tokens, ('batch', 't'))
+  result_text_tokens = self.output_token_string_from_index(result_tokens)
+  shape_checker(result_text_tokens, ('batch', 't'))
+
+  result_text = tf.strings.reduce_join(result_text_tokens,
+                                       axis=1, separator=' ')
+  shape_checker(result_text, ('batch'))
+
+  result_text = tf.strings.strip(result_text)
+  shape_checker(result_text, ('batch',))
+  return result_text
+
+Translator.tokens_to_text = tokens_to_text
+
+#inputting random ids to see what it generates
+example_output_tokens = tf.random.uniform(
+    shape=[5, 2], minval=0, dtype=tf.int64,
+    maxval=output_text_processor.vocabulary_size())
+print(translator.tokens_to_text(example_output_tokens).numpy())
+
+#takes decoder's output to sample the word predicted
+def sample(self, logits, temperature):
+  shape_checker = ShapeChecker()
+  # 't' is usually 1 here.
+  shape_checker(logits, ('batch', 't', 'vocab'))
+  shape_checker(self.token_mask, ('vocab',))
+
+  token_mask = self.token_mask[tf.newaxis, tf.newaxis, :]
+  shape_checker(token_mask, ('batch', 't', 'vocab'), broadcast=True)
+
+  # Set the logits for all masked tokens to -inf, so they are never chosen.
+  logits = tf.where(self.token_mask, -np.inf, logits)
+
+  if temperature == 0.0:
+    new_tokens = tf.argmax(logits, axis=-1)
+  else:
+    logits = tf.squeeze(logits, axis=1)
+    new_tokens = tf.random.categorical(logits/temperature,
+                                        num_samples=1)
+
+  shape_checker(new_tokens, ('batch', 't'))
+
+  return new_tokens
+
+Translator.sample = sample
+
+#test running on example
+example_logits = tf.random.normal([5, 1, output_text_processor.vocabulary_size()])
+example_output_tokens = translator.sample(example_logits, temperature=1.0)
+print(example_output_tokens)
+
+#the translating loop
+def translate_unrolled(self,
+                       input_text, *,
+                       max_length=50,
+                       return_attention=True,
+                       temperature=1.0):
+  batch_size = tf.shape(input_text)[0]
+  input_tokens = self.input_text_processor(input_text)
+  enc_output, enc_state = self.encoder(input_tokens)
+
+  dec_state = enc_state
+  new_tokens = tf.fill([batch_size, 1], self.start_token)
+
+  result_tokens = []
+  attention = []
+  done = tf.zeros([batch_size, 1], dtype=tf.bool)
+
+  for _ in range(max_length):
+    dec_input = DecoderInput(new_tokens=new_tokens,
+                             enc_output=enc_output,
+                             mask=(input_tokens!=0))
+
+    dec_result, dec_state = self.decoder(dec_input, state=dec_state)
+
+    attention.append(dec_result.attention_weights)
+
+    new_tokens = self.sample(dec_result.logits, temperature)
+
+    # If a sequence produces an `end_token`, set it `done`
+    done = done | (new_tokens == self.end_token)
+    # Once a sequence is done it only produces 0-padding.
+    new_tokens = tf.where(done, tf.constant(0, dtype=tf.int64), new_tokens)
+
+    # Collect the generated tokens
+    result_tokens.append(new_tokens)
+
+    if tf.executing_eagerly() and tf.reduce_all(done):
+      break
+
+  # Convert the list of generates token ids to a list of strings.
+  result_tokens = tf.concat(result_tokens, axis=-1)
+  result_text = self.tokens_to_text(result_tokens)
+
+  if return_attention:
+    attention_stack = tf.concat(attention, axis=1)
+    return {'text': result_text, 'attention': attention_stack}
+  else:
+    return {'text': result_text}
+Translator.translate = translate_unrolled
+
+input_text = tf.constant([
+    'i like tom',
+    'where are you'
+])
+
+result = translator.translate(
+    input_text = input_text)
+
+print(result['text'][0].numpy().decode())
+print(result['text'][1].numpy().decode())
+print()
