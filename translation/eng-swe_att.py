156235b013f68dab2bca99ebe8f23aba47735a6c
prashu316 <prashanth.panther@gmail.com>
2022-03-18 20:00:21 +0530

    training done

--- translation/eng-swe_att.py
+++ translation/eng-swe_att.py
@@ -120,6 +120,7 @@ tokens = input_vocab[example_tokens[0].numpy()]
 print(' '.join(tokens))
 
 #the returned token ids are 0 padded
+'''
 plt.subplot(1, 2, 1)
 plt.pcolormesh(example_tokens)
 plt.title('Token IDs')
@@ -127,6 +128,7 @@ plt.title('Token IDs')
 plt.subplot(1, 2, 2)
 plt.pcolormesh(example_tokens != 0)
 plt.title('Mask')
+'''
 #plt.show()
 
 embedding_dim = 256
@@ -239,3 +241,350 @@ context_vector, attention_weights = attention_layer(
 print(f'Attention result shape: (batch_size, query_seq_length, units):           {context_vector.shape}')
 print(f'Attention weights shape: (batch_size, query_seq_length, value_seq_length): {attention_weights.shape}')
 
+#attention weights and masks graph
+'''
+plt.subplot(1, 2, 1)
+plt.pcolormesh(attention_weights[:, 0, :])
+plt.title('Attention weights')
+
+plt.subplot(1, 2, 2)
+plt.pcolormesh(example_tokens != 0)
+plt.title('Mask')
+plt.show()
+'''
+
+#THE DECODER
+
+class Decoder(tf.keras.layers.Layer):
+  def __init__(self, output_vocab_size, embedding_dim, dec_units):
+    super(Decoder, self).__init__()
+    self.dec_units = dec_units
+    self.output_vocab_size = output_vocab_size
+    self.embedding_dim = embedding_dim
+
+    # For Step 1. The embedding layer convets token IDs to vectors
+    self.embedding = tf.keras.layers.Embedding(self.output_vocab_size,
+                                               embedding_dim)
+
+    # For Step 2. The RNN keeps track of what's been generated so far.
+    self.gru = tf.keras.layers.GRU(self.dec_units,
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_initializer='glorot_uniform')
+
+    # For step 3. The RNN output will be the query for the attention layer.
+    self.attention = BahdanauAttention(self.dec_units)
+
+    # For step 4. Eqn. (3): converting `ct` to `at`
+    self.Wc = tf.keras.layers.Dense(dec_units, activation=tf.math.tanh,
+                                    use_bias=False)
+
+    # For step 5. This fully connected layer produces the logits for each
+    # output token.
+    self.fc = tf.keras.layers.Dense(self.output_vocab_size)
+
+class DecoderInput(typing.NamedTuple):
+  new_tokens: Any
+  enc_output: Any
+  mask: Any
+
+class DecoderOutput(typing.NamedTuple):
+  logits: Any
+  attention_weights: Any
+
+def call(self,
+         inputs: DecoderInput,
+         state=None) -> Tuple[DecoderOutput, tf.Tensor]:
+  shape_checker = ShapeChecker()
+  shape_checker(inputs.new_tokens, ('batch', 't'))
+  shape_checker(inputs.enc_output, ('batch', 's', 'enc_units'))
+  shape_checker(inputs.mask, ('batch', 's'))
+
+  if state is not None:
+    shape_checker(state, ('batch', 'dec_units'))
+
+  # Step 1. Lookup the embeddings
+  vectors = self.embedding(inputs.new_tokens)
+  shape_checker(vectors, ('batch', 't', 'embedding_dim'))
+
+  # Step 2. Process one step with the RNN
+  rnn_output, state = self.gru(vectors, initial_state=state)
+
+  shape_checker(rnn_output, ('batch', 't', 'dec_units'))
+  shape_checker(state, ('batch', 'dec_units'))
+
+  # Step 3. Use the RNN output as the query for the attention over the
+  # encoder output.
+  context_vector, attention_weights = self.attention(
+      query=rnn_output, value=inputs.enc_output, mask=inputs.mask)
+  shape_checker(context_vector, ('batch', 't', 'dec_units'))
+  shape_checker(attention_weights, ('batch', 't', 's'))
+
+  # Step 4. Eqn. (3): Join the context_vector and rnn_output
+  #     [ct; ht] shape: (batch t, value_units + query_units)
+  context_and_rnn_output = tf.concat([context_vector, rnn_output], axis=-1)
+
+  # Step 4. Eqn. (3): `at = tanh(Wc@[ct; ht])`
+  attention_vector = self.Wc(context_and_rnn_output)
+  shape_checker(attention_vector, ('batch', 't', 'dec_units'))
+
+  # Step 5. Generate logit predictions:
+  logits = self.fc(attention_vector)
+  shape_checker(logits, ('batch', 't', 'output_vocab_size'))
+
+  return DecoderOutput(logits, attention_weights), state
+
+Decoder.call = call
+
+decoder = Decoder(output_text_processor.vocabulary_size(),
+                  embedding_dim, units)
+
+
+# Convert the target sequence, and collect the "[START]" tokens
+example_output_tokens = output_text_processor(example_target_batch)
+
+start_index = output_text_processor.get_vocabulary().index('[START]')
+first_token = tf.constant([[start_index]] * example_output_tokens.shape[0])
+
+# Run the decoder
+dec_result, dec_state = decoder(
+    inputs = DecoderInput(new_tokens=first_token,
+                          enc_output=example_enc_output,
+                          mask=(example_tokens != 0)),
+    state = example_enc_state
+)
+
+print(f'logits shape: (batch_size, t, output_vocab_size) {dec_result.logits.shape}')
+print(f'state shape: (batch_size, dec_units) {dec_state.shape}')
+
+sampled_token = tf.random.categorical(dec_result.logits[:, 0, :], num_samples=1)
+vocab = np.array(output_text_processor.get_vocabulary())
+first_word = vocab[sampled_token.numpy()]
+#print(first_word[:5])
+
+#sampling the next tokens by passing decoder's previous state along with the sampled token
+dec_result, dec_state = decoder(
+    DecoderInput(sampled_token,
+                 example_enc_output,
+                 mask=(example_tokens != 0)),
+    state=dec_state)
+sampled_token = tf.random.categorical(dec_result.logits[:, 0, :], num_samples=1)
+first_word = vocab[sampled_token.numpy()]
+#print(first_word[:5])
+
+#LOSS-1 Masked loss
+class MaskedLoss(tf.keras.losses.Loss):
+  def __init__(self):
+    self.name = 'masked_loss'
+    self.loss = tf.keras.losses.SparseCategoricalCrossentropy(
+        from_logits=True, reduction='none')
+
+  def __call__(self, y_true, y_pred):
+    shape_checker = ShapeChecker()
+    shape_checker(y_true, ('batch', 't'))
+    shape_checker(y_pred, ('batch', 't', 'logits'))
+
+    # Calculate the loss for each item in the batch.
+    loss = self.loss(y_true, y_pred)
+    shape_checker(loss, ('batch', 't'))
+
+    # Mask off the losses on padding.
+    mask = tf.cast(y_true != 0, tf.float32)
+    shape_checker(mask, ('batch', 't'))
+    loss *= mask
+
+    # Return the total.
+    return tf.reduce_sum(loss)
+
+#creating a model class for training and inference
+class TrainTranslator(tf.keras.Model):
+  def __init__(self, embedding_dim, units,
+               input_text_processor,
+               output_text_processor,
+               use_tf_function=True):
+    super().__init__()
+    # Build the encoder and decoder
+    encoder = Encoder(input_text_processor.vocabulary_size(),
+                      embedding_dim, units)
+    decoder = Decoder(output_text_processor.vocabulary_size(),
+                      embedding_dim, units)
+
+    self.encoder = encoder
+    self.decoder = decoder
+    self.input_text_processor = input_text_processor
+    self.output_text_processor = output_text_processor
+    self.use_tf_function = use_tf_function
+    self.shape_checker = ShapeChecker()
+
+  def train_step(self, inputs):
+    self.shape_checker = ShapeChecker()
+    if self.use_tf_function:
+      return self._tf_train_step(inputs)
+    else:
+      return self._train_step(inputs)
+
+#preprocessing input
+def _preprocess(self, input_text, target_text):
+  self.shape_checker(input_text, ('batch',))
+  self.shape_checker(target_text, ('batch',))
+
+  # Convert the text to token IDs
+  input_tokens = self.input_text_processor(input_text)
+  target_tokens = self.output_text_processor(target_text)
+  self.shape_checker(input_tokens, ('batch', 's'))
+  self.shape_checker(target_tokens, ('batch', 't'))
+
+  # Convert IDs to masks.
+  input_mask = input_tokens != 0
+  self.shape_checker(input_mask, ('batch', 's'))
+
+  target_mask = target_tokens != 0
+  self.shape_checker(target_mask, ('batch', 't'))
+
+  return input_tokens, input_mask, target_tokens, target_mask
+
+TrainTranslator._preprocess = _preprocess
+
+#the training step
+def _train_step(self, inputs):
+  input_text, target_text = inputs
+
+  (input_tokens, input_mask,
+   target_tokens, target_mask) = self._preprocess(input_text, target_text)
+
+  max_target_length = tf.shape(target_tokens)[1]
+
+  with tf.GradientTape() as tape:
+    # Encode the input
+    enc_output, enc_state = self.encoder(input_tokens)
+    self.shape_checker(enc_output, ('batch', 's', 'enc_units'))
+    self.shape_checker(enc_state, ('batch', 'enc_units'))
+
+    # Initialize the decoder's state to the encoder's final state.
+    # This only works if the encoder and decoder have the same number of
+    # units.
+    dec_state = enc_state
+    loss = tf.constant(0.0)
+
+    for t in tf.range(max_target_length-1):
+      # Pass in two tokens from the target sequence:
+      # 1. The current input to the decoder.
+      # 2. The target for the decoder's next prediction.
+      new_tokens = target_tokens[:, t:t+2]
+      step_loss, dec_state = self._loop_step(new_tokens, input_mask,
+                                             enc_output, dec_state)
+      loss = loss + step_loss
+
+    # Average the loss over all non padding tokens.
+    average_loss = loss / tf.reduce_sum(tf.cast(target_mask, tf.float32))
+
+  # Apply an optimization step
+  variables = self.trainable_variables
+  gradients = tape.gradient(average_loss, variables)
+  self.optimizer.apply_gradients(zip(gradients, variables))
+
+  # Return a dict mapping metric names to current value
+  return {'batch_loss': average_loss}
+
+TrainTranslator._train_step = _train_step
+
+def _loop_step(self, new_tokens, input_mask, enc_output, dec_state):
+  input_token, target_token = new_tokens[:, 0:1], new_tokens[:, 1:2]
+
+  # Run the decoder one step.
+  decoder_input = DecoderInput(new_tokens=input_token,
+                               enc_output=enc_output,
+                               mask=input_mask)
+
+  dec_result, dec_state = self.decoder(decoder_input, state=dec_state)
+  self.shape_checker(dec_result.logits, ('batch', 't1', 'logits'))
+  self.shape_checker(dec_result.attention_weights, ('batch', 't1', 's'))
+  self.shape_checker(dec_state, ('batch', 'dec_units'))
+
+  # `self.loss` returns the total for non-padded tokens
+  y = target_token
+  y_pred = dec_result.logits
+  step_loss = self.loss(y, y_pred)
+
+  return step_loss, dec_state
+TrainTranslator._loop_step = _loop_step
+
+
+translator = TrainTranslator(
+    embedding_dim, units,
+    input_text_processor=input_text_processor,
+    output_text_processor=output_text_processor,
+    use_tf_function=False)
+
+# Configure the loss and optimizer
+translator.compile(
+    optimizer=tf.optimizers.Adam(),
+    loss=MaskedLoss(),
+)
+#Where the loss should start for translation problems
+#print(np.log(output_text_processor.vocabulary_size()))
+
+'''
+for n in range(10):
+  print(translator.train_step([example_input_batch, example_target_batch]))
+print()
+'''
+
+#train using tf function to increase performance
+@tf.function(input_signature=[[tf.TensorSpec(dtype=tf.string, shape=[None]),
+                               tf.TensorSpec(dtype=tf.string, shape=[None])]])
+def _tf_train_step(self, inputs):
+  return self._train_step(inputs)
+
+TrainTranslator._tf_train_step = _tf_train_step
+translator.use_tf_function = True
+#translator.train_step([example_input_batch, example_target_batch])
+'''
+for n in range(10):
+  print(translator.train_step([example_input_batch, example_target_batch]))
+print()
+'''
+
+#checking loss on example batch
+'''
+losses = []
+for n in range(50):
+  print('.', end='')
+  logs = translator.train_step([example_input_batch, example_target_batch])
+  losses.append(logs['batch_loss'].numpy())
+
+print()
+plt.plot(losses)
+plt.show()
+'''
+
+#fresh copy of model to train on input
+train_translator = TrainTranslator(
+    embedding_dim, units,
+    input_text_processor=input_text_processor,
+    output_text_processor=output_text_processor)
+
+# Configure the loss and optimizer
+train_translator.compile(
+    optimizer=tf.optimizers.Adam(),
+    loss=MaskedLoss(),
+)
+
+#training the model
+class BatchLogs(tf.keras.callbacks.Callback):
+  def __init__(self, key):
+    self.key = key
+    self.logs = []
+
+  def on_train_batch_end(self, n, logs):
+    self.logs.append(logs[self.key])
+
+batch_loss = BatchLogs('batch_loss')
+
+
+train_translator.fit(dataset, epochs=10, callbacks=[batch_loss])
+train_translator.save_weights('attention_model_2')
+#train_translator.load_weights('attention_model_1')
+'''
+model.load_weights('./checkpoints/my_checkpoint')
+'''
